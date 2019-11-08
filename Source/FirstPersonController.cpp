06545d7f7f75154355770df1f6387341bce075a7
AtkinsJM <github@joshatkins.co.uk>
2019-11-08 21:22:06 +0000

    Refactored player start position code, and added comments to FirstPersonController class.

--- Source/FirstPersonController.cpp
+++ Source/FirstPersonController.cpp
@@ -31,18 +31,12 @@ void FirstPersonController::Attach()
 
 	Entity* playerStart = World::GetCurrent()->FindEntity("Player Start");
 	
-	if(playerStart != nullptr)
-	{
-		entity->SetPosition(playerStart->GetPosition());
-	}
-	else
-	{
-		entity->SetPosition(0, 2, 0);
-	}
+	Vec3 playerPos = playerStart ? playerStart->GetPosition() : Vec3(0, 2, 0);
+
+	entity->SetPosition(playerPos);
 
 	entity->SetPhysicsMode(Entity::CharacterPhysics);
-
-
+	
 	entity->SetMass(70);
 }
 
@@ -52,19 +46,11 @@ void FirstPersonController::UpdateWorld()
 	HandleInput();
 }
 
-void FirstPersonController::UpdatePhysics()
-{
-
-}
-
-void FirstPersonController::Collision(Entity* entity, const Vec3& position, const Vec3& normal, float speed)
-{
-
-}
-
 void FirstPersonController::HandleInput()
 {
 	if (!gameWindow || !mainCamera) { return; }
+
+	// Get mouse movement this frame, then recentre mouse
 	Vec3 mouseDelta = gameWindow->GetMousePosition() - Vec3(screenCentre.x, screenCentre.y);
 	gameWindow->SetMousePosition(screenCentre.x, screenCentre.y);
 
@@ -72,18 +58,18 @@ void FirstPersonController::HandleInput()
 
 	float forward = movementInput.x * walkingSpeed;
 	float right = movementInput.y * walkingSpeed;
-	Print(IsGrounded());
 	float jump = IsGrounded() ? gameWindow->KeyHit(Key::Space) * jumpSpeed : 0;
 
 	float turnRotation = entity->GetRotation().y + (mouseDelta.x * turnRate);
 	float lookUpRotation = Math::Clamp(mainCamera->GetRotation().x + (mouseDelta.y * lookUpRate), -lookUpAngleClamp, lookUpAngleClamp);
 
 	bool bCrouching = gameWindow->KeyDown(Key::ControlKey);
-	Vec3 cameraPosition = mainCamera->GetPosition();
-
 	bool bSprinting = gameWindow->KeyDown(Key::Shift);
 
+	// Set camera height depending on whether or not player is crouching
+	Vec3 cameraPosition = mainCamera->GetPosition();
 	cameraPosition.y = (bCrouching && !bSprinting) ? crouchingHeight : standingHeight;
+	mainCamera->SetPosition(cameraPosition);
 
 	if (bSprinting)
 	{
@@ -98,11 +84,20 @@ void FirstPersonController::HandleInput()
 
 	entity->SetInput(turnRotation, forward, right, jump, bCrouching);
 	mainCamera->SetRotation(lookUpRotation, 0, 0);
-	mainCamera->SetPosition(cameraPosition);
 }
 
 bool FirstPersonController::IsGrounded()
 {
 	PickInfo pickinfo;
 	return World::GetCurrent()->Pick(entity->GetPosition() + Vec3(0, 0.01f, 0), entity->GetPosition() - Vec3(0, 0.01f, 0), pickinfo);
+}
+
+void FirstPersonController::UpdatePhysics()
+{
+
+}
+
+void FirstPersonController::Collision(Entity* entity, const Vec3& position, const Vec3& normal, float speed)
+{
+
 }

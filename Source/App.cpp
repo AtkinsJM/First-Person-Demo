ba48c69c6e71206fec7db17f6680aa89663f2214
AtkinsJM <github@joshatkins.co.uk>
2019-11-12 15:39:24 +0000

    Moved main game code to App class.

--- Source/App.cpp
+++ Source/App.cpp
@@ -1,126 +1,74 @@
 #include "App.h"
+#include "FirstPersonController.h"
+#include "PressurePad.h"
 
 using namespace Leadwerks;
 
-App::App() : window(NULL), context(NULL), world(NULL), camera(NULL) {}
+App::App() : window(NULL), context(NULL), world(NULL)
+{
 
-App::~App() { delete world; delete window; }
+	bUseVSync = false;
+	//camera = nullptr;
+}
+
+App::~App()
+{
+	delete world;
+	delete window;
+}
 
 bool App::Start()
 {
-	int stacksize = Interpreter::GetStackSize();
-
-	//Get the global error handler function
-	int errorfunctionindex = 0;
-#ifdef DEBUG
-	Interpreter::GetGlobal("LuaErrorHandler");
-	errorfunctionindex = Interpreter::GetStackSize();
-#endif
-
-	//Create new table and assign it to the global variable "App"
-	Interpreter::NewTable();
-	Interpreter::SetGlobal("App");
+	window = Window::Create("My Game", 0, 0, 1024, 768);
+	context = Context::Create(window);
+	world = World::Create();
 	
-	std::string scriptpath = "Scripts/Main.lua";
-	if (FileSystem::GetFileType("Scripts/App.Lua") == 1) scriptpath = "Scripts/App.Lua";
+	Map::Load("Maps/game.map");
 
-	//Invoke the start script
-	if (!Interpreter::ExecuteFile(scriptpath))
-	{
-		System::Print("Error: Failed to execute script \"" + scriptpath + "\".");
-		return false;
-	}
+	window->HideMouse();
 
-	//Call the App:Start() function
-	Interpreter::GetGlobal("App");
-	if (Interpreter::IsTable())
+	Model* player = Model::Create();
+	player->SetKeyValue("name", "Player");
+	Actor* firstPersonController = new FirstPersonController;
+	player->SetActor(firstPersonController);
+
+
+	for (int t = 0; t < World::GetCurrent()->CountEntities(); ++t)
 	{
-		Interpreter::PushString("Start");
-		Interpreter::GetTable();
-		if (Interpreter::IsFunction())
+		Entity* e = World::GetCurrent()->GetEntity(t);
+		if (e->GetKeyValue("name") != "")
 		{
-			Interpreter::PushValue(-2);//Push the app table onto the stack as "self"
-#ifdef DEBUG
-			errorfunctionindex = -(Interpreter::GetStackSize() - errorfunctionindex + 1);
-#endif
-			if (!Interpreter::Invoke(1, 1, errorfunctionindex)) return false;
-			if (Interpreter::IsBool())
-			{
-				if (!Interpreter::ToBool()) return false;
-			}
-			else
-			{
-				return false;
-			}
+			Print(e->GetKeyValue("name"));
+		}
+		else
+		{
+			Print("Entity found without name!");
 		}
 	}
+	Entity* pressurePadObj = World::GetCurrent()->FindEntity("Pressure Pad");
+	if (pressurePadObj)
+	{
+		pressurePadObj->SetKeyValue("name", "Pressure Pad");
+		Actor* pressurePad = new PressurePad;
+		pressurePadObj->SetActor(pressurePad);
+	}
+	else
+	{
+		Print("No pressure pad found!");
+	}
 
-	//Restore the stack size
-	Interpreter::SetStackSize(stacksize);
+	//Collision::SetResponse(Collision::Trigger, Collision::Prop, Collision::Trigger);
 
 	return true;
 }
 
 bool App::Loop()
 {
-	//Get the stack size
-	int stacksize = Interpreter::GetStackSize();
+	if (window->Closed() || window->KeyHit(Key::Escape)) { return false; }
 
-	//Get the global error handler function
-	int errorfunctionindex = 0;
-#ifdef DEBUG
-	Interpreter::GetGlobal("LuaErrorHandler");
-	errorfunctionindex = Interpreter::GetStackSize();
-#endif
-
-	//Call the App:Start() function
-	Interpreter::GetGlobal("App");
-	if (Interpreter::IsTable())
-	{
-		Interpreter::PushString("Loop");
-		Interpreter::GetTable();
-		if (Interpreter::IsFunction())
-		{
-			Interpreter::PushValue(-2);//Push the app table onto the stack as "self"
-#ifdef DEBUG
-			errorfunctionindex = -(Interpreter::GetStackSize() - errorfunctionindex + 1);
-#endif
-			if (!Interpreter::Invoke(1, 1, errorfunctionindex))
-			{
-				System::Print("Error: Script function App:Loop() was not successfully invoked.");
-				Interpreter::SetStackSize(stacksize);
-				return false;
-			}
-			if (Interpreter::IsBool())
-			{
-				if (!Interpreter::ToBool())
-				{
-					Interpreter::SetStackSize(stacksize);
-					return false;
-				}
-			}
-			else
-			{
-				Interpreter::SetStackSize(stacksize);
-				return false;
-			}
-		}
-		else
-		{
-			//System::Print("Error: App:Loop() function not found.");
-			Interpreter::SetStackSize(stacksize);
-			return false;
-		}
-	}
-	else
-	{
-		//System::Print("Error: App table not found.");
-		Interpreter::SetStackSize(stacksize);
-		return false;
-	}
-
-	//Restore the stack size
-	Interpreter::SetStackSize(stacksize);
-
+	Time::Update();
+	world->Update();
+	world->Render();
+	context->Sync(bUseVSync);
 	return true;
 }
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,59 +1,22 @@
 #include "Leadwerks.h"
-#include "FirstPersonController.h"
-#include "PressurePad.h"
+
+#include "App.h"
 
 using namespace Leadwerks;
 
 int main(int argc, const char* argv[])
 {
-	Window* gameWindow = Window::Create("My Game", 0, 0, 1024, 768);
-	Context* context = Context::Create(gameWindow);
-	World* world = World::Create();
-
-	bool bUseVSync = false;
-
-	Map::Load("Maps/game.map");
-
-	gameWindow->HideMouse();
-			
-	Model* player = Model::Create();
-	player->SetKeyValue("name", "Player");
-	Actor* firstPersonController = new FirstPersonController;
-	player->SetActor(firstPersonController);
-
-	for (int t = 0; t < World::GetCurrent()->CountEntities(); ++t)
+	App* app = new App;
+	if (app->Start())
 	{
-		Entity* e = World::GetCurrent()->GetEntity(t);
-		if (e->GetKeyValue("name") != "")
-		{
-			Print(e->GetKeyValue("name"));
-		}
-		else
-		{
-			Print("Entity found without name!");
-		}
-	}
-	Entity* pressurePadObj = World::GetCurrent()->FindEntity("Pressure Pad");
-	if (pressurePadObj)
-	{
-		pressurePadObj->SetKeyValue("name", "Pressure Pad");
-		Actor* pressurePad = new PressurePad;
-		pressurePadObj->SetActor(pressurePad);
+		while (app->Loop()) {}
+
+		delete app;
+
+		return 0;
 	}
 	else
 	{
-		Print("No pressure pad found!");
+		return 1;
 	}
-	Collision::SetResponse(Collision::Trigger, Collision::Prop, Collision::Trigger);
-		
-	while (true)
-	{		
-		if (gameWindow->Closed() || gameWindow->KeyHit(Key::Escape)) { return false; }
-			
-		Time::Update();
-		world->Update();
-		world->Render();
-		context->Sync(bUseVSync);
-	}
-	return 0;
 }
